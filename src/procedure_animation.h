d74f367dbf9907018b160cb593188c8733e16226
S4m1d <iams4m1d@gmail.com>
2026-05-07 21:11:17 +0000

    refactoring + CMakeLists improvement
    - moved procedural animations code to separate files
    - made make to autodetect header files

---
+++ src/procedure_animation.h
@@ -0,0 +1,7 @@
+#ifndef PROCEDURE_ANIMATION
+#define PROCEDURE_ANIMATION
+#include "driver/i2c.h"
+
+void procedure_animation_half_fill_spin(uint64_t refresh_rate_ms);
+void procedure_animation_h_line_sequence(uint64_t refresh_rate_ms);
+#endif
