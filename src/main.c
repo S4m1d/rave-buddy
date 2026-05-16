8ddaac5d3db836ed470344ec2770a804b4e9c714
S4m1d <iams4m1d@gmail.com>
2026-05-16 01:07:43 +0000

    add reading of frame from sd card

--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
 #include "animation.h"
 #include "draw.h"
+#include "sd_card.h"
 #include "driver/i2c.h"
 #include "esp_err.h"
-#include "esp_littlefs.h"
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -11,23 +11,14 @@
 #include <string.h>
 
 void app_main(void) {
-  esp_vfs_littlefs_conf_t conf = {
-      .base_path = "/littlefs",
-      .partition_label = "spiffs", // matches partitions.csv
-      .format_if_mount_failed = true,
-  };
-  esp_err_t ret = esp_vfs_littlefs_register(&conf);
-  if (ret != ESP_OK) {
-    ESP_LOGE("main", "failed to mount littlefs: %s", esp_err_to_name(ret));
-    return;
-  }
+  sd_card_init();
 
   i2c_init();
   ssd1306_init();
   draw_fill(0x00); // all pixels off
 
   while (1) {
-    animation_static("/littlefs/frame.bin");
+    animation_static("/sdcard/frame.bin");
     vTaskDelay(pdMS_TO_TICKS(2000));
     draw_fill(0x00);
     procedure_animation_nested_rectangles(500, 1);
---
+++ src/sd_card.h
@@ -0,0 +1,6 @@
+#ifndef SD_CARD_H
+#define SD_CARD_H
+
+esp_err_t sd_card_init();
+
+#endif
